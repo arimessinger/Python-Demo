6f8cd553608304848ec17cd01d812a26d31f3945
Ari  Messinger <36777386-amessinger@users.noreply.replit.com>
2024-10-01 15:51:11 +0000

    Initial commit

---
+++ file_2.py
@@ -0,0 +1,5 @@
+# def my_function(input_number):
+#   if input_number + 2 >= 4:
+#     print("The numbers added together are greater than or equal to 4!")
+#   else:
+#     print("The numbers added together aren't greater than or equal to 4!")
---
+++ main.py
@@ -0,0 +1,32 @@
+# my_string = "my string"
+# my_number = 3
+
+# print("Hello World")
+# print(my_string)
+# print(my_number)
+
+# name = input("Enter your name: ")
+# print("Hello " + name + "!")
+
+# number_1 = int(input("Enter a number: "))
+# number_2 = int(input("Enter another number: "))
+# if number_1 is number_2:
+#   print("The numbers are the same!")
+# else:
+#   print("The numbers are not the same!")
+
+# number_1 = 3
+# number_2 = 5
+# sum = number_1 + number_2
+# print("The two numbers together make " + str(sum) + "!")
+
+# import time
+
+# print("Foo")
+# time.sleep(1)
+# print("Bar")
+
+# from file_2 import my_function
+
+# number = int(input("Enter a number: "))
+# my_function(number)
